16b2aee63e4b3e3442d8cff48362d3a5534cb0da
ellymaimon <eliranmaimon@gmail.com>
2020-11-15 12:54:03 -0800

    add message event handler with temporary bitmoji URL

--- app.js
+++ app.js
@@ -6,6 +6,45 @@ const app = new App({
 	signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.event('message', async ({ event, client }) => {
+	try {
+		const bitmojiURL = process.env.BITMOJI_EXAMPLE_URL;
+
+		if (
+			event.channel_type === 'im' &&
+			event.user !== process.env.SLACK_USER_ID &&
+			!event.bot_id
+		) {
+			const { presence } = await client.users.getPresence({ user: process.env.SLACK_USER_ID });
+
+			if (presence === 'away') {
+				client.chat.postMessage({
+					token: process.env.SLACK_USER_TOKEN,
+					channel: event.channel,
+					blocks: [
+						{
+							"type": "header",
+							"text": {
+								"type": "plain_text",
+								"text": "I am offline!",
+								"emoji": true
+							}
+						},
+						{
+							"type": "image",
+							"image_url": bitmojiURL,
+							"alt_text": "Bitmoji"
+						}
+					]
+				})
+			}
+
+		}
+	}
+	catch (error) {
+		console.error(error);
+	}
+});
 
 (async () => {
 	// Start your app
